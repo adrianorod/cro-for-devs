b6dc3b414a287d250c8dce973f404907f4fb4ee5
adriano.rodrigues <adriano.rodrigues@wooza.com.br>
2019-07-23 17:42:22 -0300

    atualiza projeto

---
+++ TESTS.md
@@ -0,0 +1,11 @@
+# Testes AB
+
+Informação sobre testes AB realizados (em andamento, concluídos, etc.)
+
+[⚋] _Em andamento_
+[✔] _Aprovado_
+[✘] _Reprovado_
+
+## #versão - YYYY-MM-DD [⚋]
+
+Descrição do teste.
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,29 +1,53 @@
-var appController = (function() {
-    var formInputs = [
-        document.getElementById('name'),
-        document.getElementById('email'),
-        document.getElementById('tel'),
-        document.getElementById('message')
-    ];
-
-    var btnEnviar = document.getElementById('btn-enviar');
-
-    function getFormValue() {
-        return formInputs.map(function(input) {
-            return input.value;
-        });
-    }
-
-    function onClickButton(event) {
-        event.preventDefault();
-        console.log(getFormValue());
+const utils = (($wd) => {
+    function _getQueryString(search) {
+        let hashes = search.slice(search.indexOf('?') + 1).split('&');
+        return hashes.reduce((params, hash) => {
+            let [key, val] = hash.split('=');
+            return Object.assign(params, {[key]: decodeURIComponent(val)});
+        }, {});
     }
 
     return {
-        init: function() {
-            btnEnviar.addEventListener('click', onClickButton);
+        getQueryString: _getQueryString($wd.location.search),
+    };
+})(window);
+
+const appController = (($dc) => {
+    this.state = {
+        form: $dc.querySelector('form'),
+        formInputs: [
+            $dc.querySelector('#name'),
+            $dc.querySelector('#email'),
+            $dc.querySelector('#tel'),
+            $dc.querySelector('#message'),
+        ],
+        btnEnviar: $dc.querySelector('#btn-enviar'),
+        feedbackField: $dc.querySelector('.feedback'),
+    };
+
+    function _getFormValue() {
+        return state.formInputs.map((input) => input.value);
+    }
+
+    function _onClickButton(event) {
+        event.preventDefault();
+        _setFeedbackMsg('Dados salvos com sucesso!');
+        console.log(_getFormValue());
+    }
+
+    function _setFeedbackMsg(value) {
+        if (typeof value !== 'string') return false;
+        state.feedbackField.innerHTML = value;
+        setTimeout(() => {
+            state.feedbackField.innerHTML = '';
+        }, 5000);
+    }
+
+    return {
+        init: () => {
+            state.btnEnviar.addEventListener('click', _onClickButton);
         },
     };
-})();
+})(document);
 
 appController.init();
